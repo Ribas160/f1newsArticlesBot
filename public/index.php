2b23445b157b99beb06dca49a8c1e16f9c5e7807
ribas160 <michaelromanov@MacBook-Pro-Mikhail.local>
2021-08-18 12:11:20 +0300

    create a simple version of bot

--- public/index.php
+++ public/index.php
@@ -1,10 +1,62 @@
 <?php 
 
-use App\Core\App;
+require_once __DIR__ . '/../vendor/autoload.php';
 
-header('Access-Control-Allow-Origin: *');
 
-require_once '../vendor/autoload.php';
+/**
+ * @param string $link
+ * @return void
+ */
+function sendLink(string $link): void
+{
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
 
-$app = new App();
-$app->run();
+    $telegram = new Telegram\Bot\Api($_ENV['BOT_TOKEN']);
+    $telegram->sendMessage([
+        'chat_id' => $_ENV['CHANNEL_ID'],
+        'text' => $link,
+    ]);
+}
+
+
+/**
+ * @return string
+ */
+function F1news(): string
+{
+    $pageUrl = 'https://www.f1news.ru/export/news.xml';
+    $jsonFile = __DIR__  . '/../storage.json';
+
+    $content = file_get_contents($pageUrl);
+    preg_match('/<item>.*?<link>(?<link>.*?)<\/link>/s', $content, $matches);
+
+    if (!file_exists($jsonFile)) return newLink($jsonFile, $matches['link']);
+    
+    $json = json_decode(file_get_contents($jsonFile), true);
+    if (isset($json['link']) && $json['link'] !== $matches['link']) return newLink($jsonFile, $matches['link']);
+    else return '';
+}
+
+
+/**
+ * @param string $jsonFile
+ * @param string $link
+ * @return string
+ */
+function newLink(string $jsonFile, string $link): string
+{
+    file_put_contents($jsonFile, json_encode(['link' => $link]));
+    return $link;
+}
+
+
+
+function run(): void
+{
+    $link = F1news();
+    if ($link) sendLink($link);
+}
+
+
+run();
